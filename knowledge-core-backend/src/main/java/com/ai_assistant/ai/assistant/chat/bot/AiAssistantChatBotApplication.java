a30dec9799a852ba8dd1fd4e3b40a9967dfc3684
Gururaj254 <gururajdharmashetti390@gmail.com>
2026-05-05 11:54:04 +0530

    updated chat and document controller

--- knowledge-core-backend/src/main/java/com/ai_assistant/ai/assistant/chat/bot/AiAssistantChatBotApplication.java
+++ knowledge-core-backend/src/main/java/com/ai_assistant/ai/assistant/chat/bot/AiAssistantChatBotApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AiAssistantChatBotApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AiAssistantChatBotApplication.class, args);
 	}
-
 }
